35a4fdbcd3b4b822fc798f0bf46f57ce8c457c86
Terence Dumas <dumast@awty.org>
2025-11-02 18:10:25 -0600

    nvm

--- telemtry/analysis/database/viewer_tool/src/lib/prisma/telemtry.ts
+++ telemtry/analysis/database/viewer_tool/src/lib/prisma/telemtry.ts
@@ -1,29 +1,16 @@
-// Dynamically choose the Prisma client (telemetry vs angelique) based on POSTGRES_DB
-// This keeps dev hot-reload stable by using a global singleton.
 
-import type { PrismaClient as TelemetryPrismaClient } from '../../../.prisma/telemetry-client';
-import type { PrismaClient as AngeliquePrismaClient } from '../../../.prisma/angelique-client';
-
-type AnyPrismaClient = TelemetryPrismaClient | AngeliquePrismaClient;
-
-// Decide which client to use
-const dbName = (process.env.POSTGRES_DB || '').toLowerCase();
-const useAngelique = dbName === 'angelique';
-
-// Require at runtime to avoid bundling issues in Next
-const PrismaClientCtor: new () => AnyPrismaClient = useAngelique
-  ? (require('../../../.prisma/angelique-client').PrismaClient as new () => AngeliquePrismaClient)
-  : (require('../../../.prisma/telemetry-client').PrismaClient as new () => TelemetryPrismaClient);
+// import { PrismaClient } from '../../../.prisma/telemetry-client';
+import { PrismaClient } from '../../../.prisma/angelique-client';
 
 declare global {
-  // eslint-disable-next-line no-var
-  var __unified_prisma__: AnyPrismaClient | undefined;
+  var telemtry_prisma: PrismaClient | undefined;
 }
 
-const prisma: AnyPrismaClient = global.__unified_prisma__ ?? new PrismaClientCtor();
-
-if (process.env.NODE_ENV !== 'production') {
-  global.__unified_prisma__ = prisma;
+if (!global.telemtry_prisma) {
+  global.telemtry_prisma = new PrismaClient();
 }
 
-export default prisma;
+const telemtry_prisma: PrismaClient = global.telemtry_prisma!;
+
+
+export default telemtry_prisma;
