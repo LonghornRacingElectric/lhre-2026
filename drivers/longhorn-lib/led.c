41937c0e8af5fd352989c7317c2c6ed67ef8b348
Dhairya Gupta <dhairyag23@gmail.com>
2025-11-10 11:54:07 -0600

    Create tests for Longhorn Lib
    For now, we just have LED tests. As Longhorn Lib grows, we'll have more.

--- drivers/longhorn-lib/led.c
+++ drivers/longhorn-lib/led.c
@@ -4,50 +4,7 @@
 
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
-
-static rainbow_led_t led = {};
-
-static void led_setInt(uint8_t r, uint8_t g, uint8_t b) {
-    *(led.ccr1) = r;
-    *(led.ccr2) = g;
-    *(led.ccr3) = b;
-}
-
-void led_set(float r, float g, float b) {
-    led_setInt((uint8_t)(r * 255), (uint8_t)(g * 255), (uint8_t)(b * 255));
-}
-
-void led_off() { led_setInt(0, 0, 0); }
-
-void led_rainbow(float deltaTime) {
-    // keep track of LED rainbow phase so we can step forward into the rainbow
-    static float phase = 0.0f;
-    const float RAINBOW_PHASE_RANGE = 3.0f;
-    float phase_advance =
-        deltaTime * (RAINBOW_PHASE_RANGE / RAINBOW_CYCLE_TIME_S);
-    phase += phase_advance;
-    while (phase >= RAINBOW_PHASE_RANGE) {
-        phase -= RAINBOW_PHASE_RANGE;
-    }
-
-    float r = 0.0f, g = 0.0f, b = 0.0f;
-
-    if (phase < 1.0f) {
-        r = 1.0f - phase;
-        g = phase;
-    } else if (phase < 2.0f) {
-        float p = phase - 1.0f;
-        g = 1.0f - p;
-        b = p;
-    } else {
-        float p = phase - 2.0f;
-        b = 1.0f - p;
-        r = p;
-    }
-
-    // Normalize brightness so we don't blind ourselves
-    led_set(r * 0.5f, g * 0.5f, b * 0.5f);
-}
+#include "led_base.h"
 
 void RainbowLED(void* argument) {
     const uint32_t periodTicks = pdMS_TO_TICKS(33);
@@ -68,24 +25,6 @@ void RainbowLED(void* argument) {
         led_rainbow(elapsedSeconds);
         lastRunTime = currentRunTime;
     }
-}
-
-void setup_channel(volatile uint32_t* ccr, unsigned int channel) {
-    if (ccr) {
-        // make sure we actually have a channel, and then start it
-        // we start the LED with a mid-brightness white so that we know the
-        // board is booted
-        *ccr = 125;
-        led.pwm_start(led.timer_handle, channel);
-    }
-}
-
-void led_init(const rainbow_led_t* config) {
-    led = *config;
-    // Setup all the channels
-    setup_channel(led.ccr1, led.channel1);
-    setup_channel(led.ccr2, led.channel2);
-    setup_channel(led.ccr3, led.channel3);
 }
 
 const osThreadAttr_t led_attributes = {
---
+++ drivers/longhorn-lib/tests/led_test.cpp
@@ -0,0 +1,54 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "led_base.h"
+}
+
+using ::testing::_;
+
+class PwmControllerMock {
+   public:
+    MOCK_METHOD(void, pwm_start, (void* htim, uint32_t Channel));
+};
+
+class LEDTest;
+
+static PwmControllerMock* g_mock_instance = nullptr;
+extern "C" void fake_pwm_start_trampoline(void* htim, uint32_t Channel) {
+    ASSERT_NE(g_mock_instance, nullptr)
+        << "Mock instance was not set by test fixture SetUp()";
+    g_mock_instance->pwm_start(htim, Channel);
+}
+
+class LEDTest : public ::testing::Test {
+   protected:
+    PwmControllerMock m_pwm_mock;
+    void SetUp() override { g_mock_instance = &m_pwm_mock; }
+    void TearDown() override { g_mock_instance = nullptr; }
+};
+
+TEST_F(LEDTest, LedInit_StartsAllPwmChannels) {
+    volatile uint32_t ccr1, ccr2, ccr3;
+
+    // Dummy pointer, won't actually be used
+    void* fake_timer_handle = (void*)0xABC;
+
+    rainbow_led_t led_conf = {.ccr1 = &ccr1,
+                              .ccr2 = &ccr2,
+                              .ccr3 = &ccr3,
+                              .timer_handle = fake_timer_handle,
+                              .pwm_start = fake_pwm_start_trampoline,
+                              .channel1 = 1,
+                              .channel2 = 2,
+                              .channel3 = 3};
+
+    EXPECT_CALL(m_pwm_mock, pwm_start(fake_timer_handle, led_conf.channel1))
+        .Times(1);
+    EXPECT_CALL(m_pwm_mock, pwm_start(fake_timer_handle, led_conf.channel2))
+        .Times(1);
+    EXPECT_CALL(m_pwm_mock, pwm_start(fake_timer_handle, led_conf.channel3))
+        .Times(1);
+
+    led_init(&led_conf);
+}
