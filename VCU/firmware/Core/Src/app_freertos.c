860ddf8a2e508d579c625935b0541012d2118534
Dhairya Gupta <dhairyag23@gmail.com>
2025-11-09 22:51:00 -0600

    Enable HSE @ 16MHz, USB comms work

--- VCU/firmware/Core/Src/app_freertos.c
+++ VCU/firmware/Core/Src/app_freertos.c
@@ -54,7 +54,7 @@ osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
     .name = "defaultTask",
     .priority = (osPriority_t)osPriorityNormal,
-    .stack_size = 128 * 32};
+    .stack_size = 128 * 4};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
