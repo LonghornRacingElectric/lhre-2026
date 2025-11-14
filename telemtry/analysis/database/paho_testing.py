2bda9c29bafba1a969d4797030af17400c516ddd
cyberneel <neelesh.chevuri25@gmail.com>
2025-11-13 19:16:16 -0600

    Updated paho for angelique

--- telemtry/analysis/database/paho_testing.py
+++ telemtry/analysis/database/paho_testing.py
@@ -270,14 +270,14 @@ class DataTester:
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    car_name = "Nightwatch"
+    car_name = "Angelique"  # Change to "Nightwatch" or "Angelique" as needed
     with get_db("Nightwatch") as nightwatch_session, get_db("Angelique") as angelique_session:
         db_sessions = {'Nightwatch': nightwatch_session, 'Angelique': angelique_session}
         with MQTTHandler('paho_test', db_sessions=db_sessions, target=MQTTTarget.get()) as mqtt:
             # Protobuf message testing
             dt = DataTester(mqtt=mqtt, seed=42)
             dt.send_proto_rows(
-                tables=['packet', 'dynamics', 'controls', 'pack', 'diagnostics_high', 'diagnostics_low', 'thermal'],
+                tables=['packet', 'dynamics', 'controls', 'pack', 'diagnostics', 'thermal'],
                 num_rows= 2000,
                 delay= 0.01,
                 target=car_name
