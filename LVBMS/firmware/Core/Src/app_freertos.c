a1679fd5771c6cc0f4ceb7a4a816a08ae9fff12f
Monish Madhu <monishmadhu86@gmail.com>
2025-12-14 16:45:51 -0600

    saving

--- LVBMS/firmware/Core/Src/app_freertos.c
+++ LVBMS/firmware/Core/Src/app_freertos.c
@@ -59,7 +59,7 @@ osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
   .name = "defaultTask",
   .priority = (osPriority_t) osPriorityNormal,
-  .stack_size = 128 * 4
+  .stack_size = 2048
 };
 
 /* Private function prototypes -----------------------------------------------*/
