bee988ac2329a51fd898b3665ff056a49c76fcb0
Rolandw31 <68976050+Rolandw31@users.noreply.github.com>
2026-01-21 23:17:40 -0600

    vcu can rtos

--- HVC/firmware/Core/Src/can.c
+++ HVC/firmware/Core/Src/can.c
@@ -12,6 +12,7 @@
 /* USER CODE BEGIN 0 */
 
 static msg_contactor_status_t contactor_status_tx;
+static can_message_t *contactor_status_handle;
 
 // static msg_vcu_current_sense_t vcu_current_tx;
 
@@ -26,10 +27,10 @@ static can_config_t can_config = {
     .stop_fn            = (CAN_Stop_fn)HAL_FDCAN_Stop,
     .add_to_queue_fn    = (CAN_AddToQ_fn)HAL_FDCAN_AddMessageToTxFifoQ,
     .get_rx_message_fn  = (CAN_GetRxMessage_fn)HAL_FDCAN_GetRxMessage,
-    .tick_fn            = (Tick_fn)osKernelGetTickCount,
+    .tick_fn            = osKernelGetTickCount,
     .add_filter_fn      = (CAN_AddFilter_fn)HAL_FDCAN_ConfigFilter,
-    .malloc_fn          = (Malloc_fn)pvPortMalloc,
-    .free_fn            = (Free_fn)vPortFree,
+    .malloc_fn          = pvPortMalloc,
+    .free_fn            = vPortFree,
 };
 
 
@@ -37,9 +38,6 @@ void hvc_can_init(void) {
 
     can_rtos_init(&can_config);
     can_rtos_register_interface(&can1);
-
-    can_register_interface(&can1);
-
     
     // static msg_vcu_current_sense_t msg_content;
     // can_message_t* msg = can_get_message_handle(
@@ -47,9 +45,7 @@ void hvc_can_init(void) {
     //     VCU_CURRENT_SENSE_DLC, pack_vcu_current_sense);
     // can_register_send_packet(&can1, msg);
 
-    can_service(&can1);
-
-    can_message_t* contactor_status_handle = can_get_message_handle(
+    contactor_status_handle = can_get_message_handle(
         &contactor_status_tx, CONTACTOR_STATUS_ID, CONTACTOR_STATUS_FREQ,
         CONTACTOR_STATUS_DLC, (CAN_pack_message_fn)pack_contactor_status);
 
