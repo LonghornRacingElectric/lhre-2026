a1ddde7ef12f1be3b91ab6be041e6006f61d69fd
Dhairya Gupta <dhairyag23@gmail.com>
2026-02-21 06:18:26 -0600

    Update pdu_can.c

--- PDU/firmware/pdu/pdu_can.c
+++ PDU/firmware/pdu/pdu_can.c
@@ -110,7 +110,8 @@ bool vehicle_in_drive(void) { return !vehicle_in_park(); }
  */
 bool hvc_imd_fault(void) {
     return indicator_status_mailbox.imd_error ||
-           message_timed_out(indicator_status_mailbox_handle, 1000);
+           message_timed_out(indicator_status_mailbox_handle,
+                             INDICATORS_SHUTDOWN_STATUS_TIMEOUT_MS);
 }
 
 bool hvc_bms_fault(void) {
