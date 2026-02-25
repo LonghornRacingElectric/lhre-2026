40ec976d5b7d5fc373691fd063967442ebafac3c
Dhairya Gupta <dhairyag23@gmail.com>
2026-02-24 21:02:06 -0600

    Fix DUI, PDU, and HVC CAN interfaces

--- PDU/firmware/pdu/pdu_can.c
+++ PDU/firmware/pdu/pdu_can.c
@@ -19,29 +19,16 @@ static can_interface_t data_acq_bus = {
     .handle = &hfdcan2,
 };
 
-static can_config_t pdu_can_config = {
-    .init_fn = (CAN_Init_fn)HAL_FDCAN_Init,
-    .start_fn = (CAN_Start_fn)HAL_FDCAN_Start,
-    .noti_fn = (CAN_ActivateNotifications_fn)HAL_FDCAN_ActivateNotification,
-    .stop_fn = (CAN_Stop_fn)HAL_FDCAN_Stop,
-    .add_to_queue_fn = (CAN_AddToQ_fn)HAL_FDCAN_AddMessageToTxFifoQ,
-    .get_rx_message_fn = (CAN_GetRxMessage_fn)HAL_FDCAN_GetRxMessage,
-    .tick_fn = HAL_GetTick,
-    .add_filter_fn = (CAN_AddFilter_fn)HAL_FDCAN_ConfigFilter,
-    .malloc_fn = pvPortMalloc,
-    .free_fn = vPortFree,
-};
-
 /** ==
  * CAN Packets
  *  ==
  */
 
 static msg_indicators_shutdown_status_t indicator_status_mailbox = {0};
-static can_receive_message_t* indicator_status_mailbox_handle = NULL;
+static can_receive_message_t *indicator_status_mailbox_handle = NULL;
 
 static msg_dui_r2d_authorization_t r2d_authorization_mailbox = {0};
-static can_receive_message_t* r2d_authorization_mailbox_handle = NULL;
+static can_receive_message_t *r2d_authorization_mailbox_handle = NULL;
 
 void pdu_can_add_receive_handlers(void);
 
@@ -50,18 +37,37 @@ void pdu_can_add_receive_handlers(void);
  * handlers. Also starts the CAN transceiver and receiver tasks.
  */
 void pdu_can_init(void) {
-    can_rtos_init(&pdu_can_config);
+  can_config_t pdu_can_config = {
+      .init_fn = (CAN_Init_fn)HAL_FDCAN_Init,
+      .start_fn = (CAN_Start_fn)HAL_FDCAN_Start,
+      .noti_fn = (CAN_ActivateNotifications_fn)HAL_FDCAN_ActivateNotification,
+      .stop_fn = (CAN_Stop_fn)HAL_FDCAN_Stop,
+      .add_to_queue_fn = (CAN_AddToQ_fn)HAL_FDCAN_AddMessageToTxFifoQ,
+      .get_tx_fifo_free_level_fn =
+          (CAN_GetTxFifoFreeLevel_fn)HAL_FDCAN_GetTxFifoFreeLevel,
+      .get_rx_message_fn = (CAN_GetRxMessage_fn)HAL_FDCAN_GetRxMessage,
+      .tick_fn = HAL_GetTick,
+      .add_filter_fn = (CAN_AddFilter_fn)HAL_FDCAN_ConfigFilter,
+      .malloc_fn = pvPortMalloc,
+      .free_fn = vPortFree,
+      .init_bit = FDCAN_CCCR_INIT,
+  };
 
-    // Register physical interfaces FIRST
-    can_rtos_register_interface(&critical_bus);
-    can_rtos_register_interface(&data_acq_bus);
+  can_rtos_init(&pdu_can_config);
 
-    pdu_can_add_receive_handlers();
+  critical_bus.cccr_reg = &hfdcan1.Instance->CCCR;
+  data_acq_bus.cccr_reg = &hfdcan2.Instance->CCCR;
 
-    can_rtos_start_transceiver_task(osPriorityNormal);
-    can_rtos_start_receiver_task(osPriorityAboveNormal);
+  // Register physical interfaces FIRST
+  can_rtos_register_interface(&critical_bus);
+  can_rtos_register_interface(&data_acq_bus);
 
-    log_printf(LOG_INFO, "[PDU] CAN RTOS initialized\n");
+  pdu_can_add_receive_handlers();
+
+  can_rtos_start_transceiver_task(osPriorityNormal);
+  can_rtos_start_receiver_task(osPriorityAboveNormal);
+
+  log_printf(LOG_INFO, "[PDU] CAN RTOS initialized\n");
 }
 
 /**
@@ -72,30 +78,30 @@ void pdu_can_init(void) {
  *
  */
 void pdu_can_add_receive_handlers(void) {
-    // Indicators + Shutdown Status
-    indicator_status_mailbox_handle = can_get_receive_message_handle(
-        &indicator_status_mailbox, INDICATORS_SHUTDOWN_STATUS_ID,
-        (CAN_unpack_message_fn)unpack_indicators_shutdown_status);
+  // Indicators + Shutdown Status
+  indicator_status_mailbox_handle = can_get_receive_message_handle(
+      &indicator_status_mailbox, INDICATORS_SHUTDOWN_STATUS_ID,
+      (CAN_unpack_message_fn)unpack_indicators_shutdown_status);
 
-    can_rtos_register_receive_packet(&critical_bus,
-                                     indicator_status_mailbox_handle);
+  can_rtos_register_receive_packet(&critical_bus,
+                                   indicator_status_mailbox_handle);
 
-    log_printf(LOG_INFO, "[PDU] CAN IMD + BMS handlers registered\n");
+  log_printf(LOG_INFO, "[PDU] CAN IMD + BMS handlers registered\n");
 
-    // DUI R2D Authorization
-    r2d_authorization_mailbox_handle = can_get_receive_message_handle(
-        &r2d_authorization_mailbox, DUI_R2D_AUTHORIZATION_ID,
-        (CAN_unpack_message_fn)unpack_dui_r2d_authorization);
+  // DUI R2D Authorization
+  r2d_authorization_mailbox_handle = can_get_receive_message_handle(
+      &r2d_authorization_mailbox, DUI_R2D_AUTHORIZATION_ID,
+      (CAN_unpack_message_fn)unpack_dui_r2d_authorization);
 
-    can_rtos_register_receive_packet(&critical_bus,
-                                     r2d_authorization_mailbox_handle);
+  can_rtos_register_receive_packet(&critical_bus,
+                                   r2d_authorization_mailbox_handle);
 
-    log_printf(LOG_INFO, "[PDU] CAN R2D Authorization handler registered\n");
+  log_printf(LOG_INFO, "[PDU] CAN R2D Authorization handler registered\n");
 }
 
 bool vehicle_in_park(void) {
-    // 0 indicates VCU has NOT authorized R2D, so vehicle is in Park.
-    return r2d_authorization_mailbox.r2d_authorized == 0;
+  // 0 indicates VCU has NOT authorized R2D, so vehicle is in Park.
+  return r2d_authorization_mailbox.r2d_authorized == 0;
 }
 
 bool vehicle_in_drive(void) { return !vehicle_in_park(); }
@@ -109,13 +115,13 @@ bool vehicle_in_drive(void) { return !vehicle_in_park(); }
  * @return false
  */
 bool hvc_imd_fault(void) {
-    return indicator_status_mailbox.imd_error ||
-           message_timed_out(indicator_status_mailbox_handle,
-                             INDICATORS_SHUTDOWN_STATUS_TIMEOUT_MS * 4);
+  return indicator_status_mailbox.imd_error ||
+         message_timed_out(indicator_status_mailbox_handle,
+                           INDICATORS_SHUTDOWN_STATUS_TIMEOUT_MS * 4);
 }
 
 bool hvc_bms_fault(void) {
-    return indicator_status_mailbox.bms_error ||
-           message_timed_out(indicator_status_mailbox_handle,
-                             INDICATORS_SHUTDOWN_STATUS_TIMEOUT_MS * 4);
+  return indicator_status_mailbox.bms_error ||
+         message_timed_out(indicator_status_mailbox_handle,
+                           INDICATORS_SHUTDOWN_STATUS_TIMEOUT_MS * 4);
 }
