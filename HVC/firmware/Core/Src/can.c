4461897ef44a4f473feaee255ec2b5b2d519b50a
Rolandw31 <68976050+Rolandw31@users.noreply.github.com>
2026-04-11 13:06:46 -0500

    can changes

--- HVC/firmware/Core/Src/can.c
+++ HVC/firmware/Core/Src/can.c
@@ -59,12 +59,6 @@ void hvc_can_init(void) {
   can_rtos_init(&can_config);
   can_rtos_register_interface(&critical_can_bus);
 
-  // static msg_vcu_current_sense_t msg_content;
-  // can_message_t* msg = can_get_message_handle(
-  //     &msg_content, VCU_CURRENT_SENSE_ID, VCU_CURRENT_SENSE_FREQ,
-  //     VCU_CURRENT_SENSE_DLC, pack_vcu_current_sense);
-  // can_register_send_packet(&critical_can_bus, msg);
-
   // TX: contactor status
   can_message_t *contactor_status_handle = can_get_message_handle(
       &contactor_status_tx, CONTACTOR_STATUS_ID, CONTACTOR_STATUS_FREQ,
