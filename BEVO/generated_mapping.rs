effd683c9036179973104c44b59a836962c7f00f
Samuel Li <samuel.li@utexas.edu>
2026-04-18 02:11:30 -0500

    update tsm packets to main

--- BEVO/generated_mapping.rs
+++ BEVO/generated_mapping.rs
@@ -145,7 +145,7 @@ pub fn update_proto_field_generated(data: &mut OrionSensorData, name: &str, val:
     }
 }
 
-pub fn update_proto_bool_generated(_data: &mut OrionSensorData, name: &str, _val: bool) {
+pub fn update_proto_bool_generated(data: &mut OrionSensorData, name: &str, val: bool) {
     match name {
         _ => (),
     }
