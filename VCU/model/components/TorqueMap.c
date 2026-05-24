67e11a50cb19f3703a38e4637fd7e1a7ff703061
matt-mekha <matthew.mekha@gmail.com>
2026-05-24 01:45:41 -0500

    torque map as param

--- VCU/model/components/TorqueMap.c
+++ VCU/model/components/TorqueMap.c
@@ -10,23 +10,6 @@
 #define EFFICIENCY 0.96f
 #define PI_F                  3.14159265f
 
-// X axis: APPS (0.0 to 1.0), mapped to torque demand 0–220 Nm in 11 equal steps
-// Y axis: motor RPM (0 to 6000) in 11 equal steps of 600
-// Values = min(power-limit torque at that RPM, linear APPS torque demand)
-// Power budget: 80000 W * 0.96 efficiency = 76800 W mechanical
-static const float torque_table[LOOKUP2D_POINTS_Y][LOOKUP2D_POINTS_X] = {
-  /* rpm=    0 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 100.0f, 110.0f, 120.0f, 130.0f,  140.0f,  150.00f },
-  /* rpm=  600 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 100.0f, 110.0f, 120.0f, 130.0f,  140.0f,  150.00f },
-  /* rpm= 1200 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 120.0f, 130.0f, 150.0f,  160.0f,  170.00f },
-  /* rpm= 1800 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 120.0f, 130.0f, 150.0f,  170.0f,  190.00f },
-  /* rpm= 2400 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 132.0f, 154.0f, 176.0f,  188.0f,  200.00f },
-  /* rpm= 3000 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 132.0f, 154.0f, 176.0f,  198.0f,  220.00f },
-  /* rpm= 3600 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 132.0f, 154.0f, 176.0f,  198.0f,  203.72f },
-  /* rpm= 4200 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 132.0f, 154.0f, 174.62f, 174.62f, 174.62f },
-  /* rpm= 4800 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 132.0f, 152.79f, 152.79f, 152.79f, 152.79f },
-  /* rpm= 5400 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 132.0f, 135.81f, 135.81f, 135.81f, 135.81f },
-  /* rpm= 6000 */ {   0.0f,  22.0f,  44.0f,  66.0f,  88.0f, 110.0f, 122.23f, 122.23f, 122.23f, 122.23f, 122.23f },
-};
 static Lookup2D torque_lookup;
 
 static float calc_max_torque_at_rpm(float rpm, float max_torque_nm) {
@@ -50,14 +33,12 @@ static float calc_max_torque_at_rpm(float rpm, float max_torque_nm) {
 }
 
 void torque_map_init(const vcu_parameters_t *params) {
-  (void)params;
-
   Lookup2D_init(&torque_lookup,
                 0.0f,          // x0 = APPS min
                 1.0f,          // x1 = APPS max
                 0.0f,          // y0 = RPM min
                 MAX_MOTOR_RPM, // y1 = RPM max
-                torque_table);
+                params->torque_map.torque_map);
 }
 
 static float compute_low_cell_voltage_derate(const vcu_inputs_t *in,
