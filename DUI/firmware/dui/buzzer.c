12553c48e30e8043c1e8e3bbc23ebb3c28130001
Alice Di Mauro <alicedimauro@utexas.edu>
2026-05-24 13:40:30 -0500

    legal buzzer volume (#237)
    * buzzer louder and hardcoded with switch
    
    * listens to VCU command
    
    * trying buzzer read drive packet
    
    * r2d speaker turning on with drive state
    
    * serial print fix

---
+++ DUI/firmware/dui/buzzer.c
@@ -0,0 +1,60 @@
+#include "buzzer.h"
+
+#include "dui_can.h"
+#include "main.h"
+#include "r2d.h"
+
+#include "cmsis_os.h"
+#include <cmsis_os2.h>
+
+#define BUZZER_HALF_PERIOD_US 500U
+#define BUZZER_TEST_DURATION_MS 2000U
+static osThreadAttr_t buzzer_thread_attrs = {
+    .name = "Buzzer",
+    .priority = osPriorityAboveNormal,
+    .stack_size = 128 * 8,
+};
+
+static void buzzer_task(void *argument);
+static void delay_us(uint32_t delay);
+
+void dui_buzzer_init(void) { osThreadNew(buzzer_task, NULL, &buzzer_thread_attrs); }
+
+static void buzzer_task(void *argument) {
+  bool speaker_high = false;
+  bool r2d_buzzer_was_active = dui_r2d_buzzer_active();// || is_r2d_enabled();
+
+  CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
+  DWT->CYCCNT = 0;
+  DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
+
+  while (true) {
+    bool r2d_buzzer_active = dui_r2d_buzzer_active();// || is_r2d_enabled();
+
+    if (r2d_buzzer_active && !r2d_buzzer_was_active) {
+      uint32_t cycles =
+          (BUZZER_TEST_DURATION_MS * 1000U) / BUZZER_HALF_PERIOD_US;
+
+      for (uint32_t i = 0; i < cycles; i++) {
+        speaker_high = !speaker_high;
+        HAL_GPIO_WritePin(SPEAKER_GPIO_Port, SPEAKER_Pin,
+                          speaker_high ? GPIO_PIN_SET : GPIO_PIN_RESET);
+        delay_us(BUZZER_HALF_PERIOD_US);
+      }
+
+      speaker_high = false;
+      HAL_GPIO_WritePin(SPEAKER_GPIO_Port, SPEAKER_Pin, GPIO_PIN_RESET);
+    }
+
+    r2d_buzzer_was_active = r2d_buzzer_active;
+    osDelay(pdMS_TO_TICKS(1));
+  }
+}
+
+static void delay_us(uint32_t delay) {
+  uint32_t start = DWT->CYCCNT;
+  uint32_t cycles = (SystemCoreClock / 1000000U) * delay;
+
+  while ((DWT->CYCCNT - start) < cycles) {
+  }
+}
