0fcb8115fa3e893c41aa23d0e11bbc8e1eef8c5e
matt-mekha <matthew.mekha@gmail.com>
2026-05-25 08:13:46 -0500

    .

--- VCU/firmware/Core/Src/app_freertos.c
+++ VCU/firmware/Core/Src/app_freertos.c
@@ -169,6 +169,7 @@ static vcu_parameters_t s_params = {
 
 static vcu_model_context_t ctx = {0};
 
+
 /* USER CODE END Variables */
 /* Definitions for defaultTask */
 osThreadId_t defaultTaskHandle;
