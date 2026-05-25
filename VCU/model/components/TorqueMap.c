71df74278929fb7b126be8032a9fa9a13615495e
matt-mekha <matthew.mekha@gmail.com>
2026-05-25 09:49:47 -0500

    skidpad pedal map

--- VCU/model/components/TorqueMap.c
+++ VCU/model/components/TorqueMap.c
@@ -67,6 +67,15 @@ void torque_map_evaluate(const vcu_inputs_t *in, vcu_outputs_t *out,
 
   float available_torque = Lookup1D_evaluate(&torque_lookup, rpm);
   float pedal_percent_of_available_torque = powf(apps, params->torque_map.pedal_curve_exponent);
+
+  if(apps < 0.10f) {
+    pedal_percent_of_available_torque = apps / 0.05f * 0.30f;
+  } else if(apps < 0.30f) {
+    pedal_percent_of_available_torque = (apps - 0.05f) / (0.30f - 0.05f) * 0.40f + 0.30f;
+  } else {
+    pedal_percent_of_available_torque = 0.70f;
+  }
+  
   out->torque_lookup_output = available_torque * pedal_percent_of_available_torque;
 
   out->derate_factor_cell_voltage = compute_low_cell_voltage_derate(in, params);
