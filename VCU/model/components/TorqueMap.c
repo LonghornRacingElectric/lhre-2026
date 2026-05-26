82901d0f4296832802b5c53233107eecd4f49834
matt-mekha <matthew.mekha@gmail.com>
2026-05-26 14:30:36 -0500

    cleaned up pedal map

--- VCU/model/components/TorqueMap.c
+++ VCU/model/components/TorqueMap.c
@@ -11,6 +11,7 @@
 #define PI_F                  3.14159265f
 
 static Lookup1D torque_lookup;
+static Lookup1D pedal_lookup;
 
 static float calc_max_torque_at_rpm(float rpm, float max_torque_nm) {
   const float mech_power_w = BATTERY_POWER_W * EFFICIENCY;
@@ -37,6 +38,10 @@ void torque_map_init(const vcu_parameters_t *params) {
                 0.0f,
                 MAX_MOTOR_RPM,
                 params->torque_map.power_limit_torque);
+  Lookup1D_init(&pedal_lookup,
+                0.0f,
+                1.0f,
+                params->torque_map.pedal_map);
 }
 
 static float compute_low_cell_voltage_derate(const vcu_inputs_t *in,
@@ -66,15 +71,8 @@ void torque_map_evaluate(const vcu_inputs_t *in, vcu_outputs_t *out,
   float rpm = clamp_f(in->motor_speed_rpm, 0.0f, MAX_MOTOR_RPM);
 
   float available_torque = Lookup1D_evaluate(&torque_lookup, rpm);
-  float pedal_percent_of_available_torque = powf(apps, params->torque_map.pedal_curve_exponent);
-
-  if(apps < 0.10f) {
-    pedal_percent_of_available_torque = apps / 0.10f * 0.30f;
-  } else if(apps < 0.30f) {
-    pedal_percent_of_available_torque = (apps - 0.10f) / (0.30f - 0.10f) * 0.40f + 0.30f;
-  } else {
-    pedal_percent_of_available_torque = 0.70f;
-  }
+  float pedal_remapped = Lookup1D_evaluate(&pedal_lookup, apps);
+  float pedal_percent_of_available_torque = powf(pedal_remapped, params->torque_map.pedal_curve_exponent);
   
   out->torque_lookup_output = available_torque * pedal_percent_of_available_torque;
 
