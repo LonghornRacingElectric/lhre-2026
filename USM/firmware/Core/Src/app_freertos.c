bec8522a24bc796fd4426f8448a13ef56b4d50c8
Dhairya Gupta <dhairyag23@gmail.com>
2026-05-28 23:21:55 -0700

    Update app_freertos.c

--- USM/firmware/Core/Src/app_freertos.c
+++ USM/firmware/Core/Src/app_freertos.c
@@ -82,7 +82,7 @@ osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
     .name = "defaultTask",
     .priority = (osPriority_t)osPriorityNormal,
-    .stack_size = 128 * 4};
+    .stack_size = 512 * 4};
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
