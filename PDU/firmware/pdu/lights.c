f64ff6fa90ffdea7ef359cbedef949d573d5a974
alicedimauro <alicedimauro@utexas.edu>
2026-05-30 15:40:35 -0500

    more timeout wait

--- PDU/firmware/pdu/lights.c
+++ PDU/firmware/pdu/lights.c
@@ -80,7 +80,7 @@ static void set_tssi_fault(uint32_t tick) {
 }
 
 static void set_tssi_no_comms(uint32_t tick, uint32_t startup_tick) {
-  const uint32_t startup_grace_period_ms = 3000U;
+  const uint32_t startup_grace_period_ms = 3500U;
 
   if (tick - startup_tick < startup_grace_period_ms) {
     set_tssi_normal();
