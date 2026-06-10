2ecc7f5f257a07cb529292bd1ca979e73c92f0e6
matt-mekha <matthew.mekha@gmail.com>
2026-06-10 01:10:43 -0500

    buzz only during can packet = 1

--- DUI/firmware/dui/buzzer.c
+++ DUI/firmware/dui/buzzer.c
@@ -7,7 +7,6 @@
 #include <cmsis_os2.h>
 
 #define BUZZER_HALF_PERIOD_US 500U
-#define BUZZER_TEST_DURATION_MS 2000U
 static osThreadAttr_t buzzer_thread_attrs = {
     .name = "Buzzer",
     .priority = osPriorityAboveNormal,
@@ -21,32 +20,22 @@ void dui_buzzer_init(void) { osThreadNew(buzzer_task, NULL, &buzzer_thread_attrs
 
 static void buzzer_task(void *argument) {
   bool speaker_high = false;
-  bool r2d_buzzer_was_active = dui_r2d_buzzer_active();
 
   CoreDebug->DEMCR |= CoreDebug_DEMCR_TRCENA_Msk;
   DWT->CYCCNT = 0;
   DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
   while (true) {
-    bool r2d_buzzer_active = dui_r2d_buzzer_active();
-
-    if (r2d_buzzer_active && !r2d_buzzer_was_active) {
-      uint32_t cycles =
-          (BUZZER_TEST_DURATION_MS * 1000U) / BUZZER_HALF_PERIOD_US;
-
-      for (uint32_t i = 0; i < cycles; i++) {
-        speaker_high = !speaker_high;
-        HAL_GPIO_WritePin(SPEAKER_GPIO_Port, SPEAKER_Pin,
-                          speaker_high ? GPIO_PIN_SET : GPIO_PIN_RESET);
-        delay_us(BUZZER_HALF_PERIOD_US);
-      }
-
+    if (dui_r2d_buzzer_active()) {
+      speaker_high = !speaker_high;
+      HAL_GPIO_WritePin(SPEAKER_GPIO_Port, SPEAKER_Pin,
+                        speaker_high ? GPIO_PIN_SET : GPIO_PIN_RESET);
+      delay_us(BUZZER_HALF_PERIOD_US);
+    } else {
       speaker_high = false;
       HAL_GPIO_WritePin(SPEAKER_GPIO_Port, SPEAKER_Pin, GPIO_PIN_RESET);
+      osDelay(pdMS_TO_TICKS(1));
     }
-
-    r2d_buzzer_was_active = r2d_buzzer_active;
-    osDelay(pdMS_TO_TICKS(1));
   }
 }
 
